062bd0c24113b421c5bead4fb4640c0aca39f927
ᴋɪᴅ ꜱʟᴀꜱʜᴇʀ <87477295+JoyBoySer@users.noreply.github.com>
2023-12-08 23:17:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ module.exports = {
         API_KEY: process.env.HEROKU_API_KEY,
         APP_NAME: process.env.HEROKU_APP_NAME
     },
-    BASE_URL : "https://inrl-web-fkns.onrender.com/",
+    BASE_URL : "https://bit.ly/m/SHADOW-MD",
     BGM_URL : process.env.BGM_URL || "null",
     REJECT_CALL : toBool(process.env.REJECT_CALL || 'false'),
     BADWORD_BLOCK : toBool(process.env.BADWORD_BLOCK || 'false'),
